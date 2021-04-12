d266367bf3a4ce014f08737a6ec69933d010b102
Duong Tuan <tdhhxn@umsystem.edu>
2021-04-11 19:31:18 -0500

    Implement option function

--- findc.c
+++ findc.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 void help(){
 	printf("The options for this program are: \n");
 	printf("-f: input filename (required)\n");
@@ -11,7 +11,7 @@ void help(){
 
 
 int main(int argc, char** argv){
-	char* inputFile = NULL;
+	char * inputFile = NULL;
 	char targetChar = 'c';
 	int option;
 	while( (option = getopt(argc, argv, "hf:c:")) != -1){
@@ -25,9 +25,20 @@ int main(int argc, char** argv){
 			case 'c':
 				targetChar = optarg;
 				break;		
-			case '?': 
+			case '?':
+				if (optopt == 'c')
+					fprintf (stderr, "Option -%c requires an argument.\n",optopt);
+				else if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c`.\n", optopt);
+				else
+					fprintf(stderr, "Unknow option character `\\x%x`. \n", optopt);
+				return EXIT_FAILURE;
+			default:
+				help();
+				break; 
 		}
 
 	}
 
+	return EXIT_SUCCESS;
 }
--- findc.h
+++ findc.h
@@ -1,1 +1,2 @@
-:
+void help();
+int main(int, char**);
